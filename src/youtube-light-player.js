ad4e0c95bbfc0398796e5144ce4496c0b293ce70
Alexander Pleshakov <sh.plh@yandex.ru>
2020-05-02 18:18:56 +0300

    set watched attributes for element

--- src/youtube-light-player.js
+++ src/youtube-light-player.js
@@ -1,4 +1,10 @@
 class YoutubeLightPlayer extends HTMLElement {
+
+  //watched attributes
+  static get observedAttributes() {
+    return ['src'];
+  }
+
 }
 
 customElements.define('youtube-light-player', YoutubeLightPlayer);
