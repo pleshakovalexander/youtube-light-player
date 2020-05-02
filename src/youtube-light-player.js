03ac28ae3979e171dfc44d57fb541df6c5e5f298
Alexander Pleshakov <sh.plh@yandex.ru>
2020-05-02 20:18:18 +0300

    add play button to component

--- src/youtube-light-player.js
+++ src/youtube-light-player.js
@@ -25,6 +25,12 @@ class YoutubeLightPlayer extends HTMLElement {
         `
           <div class="video">
             <img class="video__thumbnail" src="https://i.ytimg.com/vi/${this.videoId}/sddefault.jpg">
+            <button type="button" class="video__button" >
+              <svg width="68" height="48" viewBox="0 0 68 48">
+                <path class="video__button-shape" fill="#212121" fill-opacity="0.8" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"></path>
+                <path d="M 45,24 27,14 27,34" fill="#fff"></path>
+              </svg>
+            </button>
           </div>
           <style>
             .video {
@@ -40,6 +46,23 @@ class YoutubeLightPlayer extends HTMLElement {
               display:block;
               margin: -9.5% 0;
             }
+            .video__button{
+              position: absolute;
+              padding: 0;
+              width: 68px;
+              height: 48px;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              background: none;
+              border: none;
+              cursor: pointer;
+            } 
+            .video:hover .video__button-shape,
+            .video__button:focus .video__button-shape {
+              fill-opacity: 0.8;
+              fill: #ff0000;
+            } 
           </style>
         `;
     }
